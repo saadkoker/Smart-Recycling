081239f648737e964fa7b3c55bb4dd4b9d37b7e9
saadkoker <60635704+saadkoker@users.noreply.github.com>
2022-05-28 20:13:05 -0400

    Changed classifier

--- Bin Classifier/Model/Python/classifier.py
+++ Bin Classifier/Model/Python/classifier.py
@@ -9,7 +9,7 @@ labels = ["compost", "recycling", "garbage", "mixed_containers", "mixed_paper"]
 # Load the model
 model = load_model('keras_model.h5', compile=False)
 
-def predict(img_path):
+def predict(img_path) --> str:
     # Create the array of the right shape to feed into the keras model
     # The 'length' or number of images you can put into the array is
     # determined by the first position in the shape tuple, in this case 1.
@@ -34,7 +34,8 @@ def predict(img_path):
     counter = 0
     for confidence in prediction.flatten():
         if (confidence*100) > conf_threshold:
-            print(labels[counter])
+            return(labels[counter])
             break
         else:
             counter += 1
+
